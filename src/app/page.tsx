b7e0e0cb0ba3dfcfa7210c92bba7b29b06e433bc
Rodrigo Yuji Seto Soma <rodrigo.y.seto@gmail.com>
2026-01-07 19:47:11 +0900

    feat: create and style interactive question component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { QuestionItem } from "@/components/QuestionItem";
 import { questions } from "@/data/questions";
 import { useState } from "react";
 
@@ -7,16 +8,17 @@ const Page = () => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const title = "Quiz";
 
+  const handleAnswered = (answer: number) => {};
+
   return (
-    
     <div className="w-full h-screen flex justify-center items-center bg-gray-800">
       <div className="w-full max-w-xl rounded-md bg-gray-900 text-white shadow shadow-black">
-        <div className="p-5 font-bold text-2xl border-b border-gray-500">{title}</div>
+        <div className="p-5 text-center font-bold text-3xl border-b border-gray-700">{title}</div>
         <div className="p-5">
-          ...
+          <QuestionItem question={questions[currentQuestion]} count={currentQuestion + 1} onAnswer={handleAnswered} />
         </div>
-        <div className="p-5 text-center border-t border-gray-500">
-          {currentQuestion + 1} of {questions.length} question{questions.length === 1 ? '' : 's'}
+        <div className="p-5 text-center border-t border-gray-700">
+          {currentQuestion + 1} of {questions.length} question{questions.length === 1 ? "" : "s"}
         </div>
       </div>
     </div>
