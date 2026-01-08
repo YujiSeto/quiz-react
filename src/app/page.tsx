60261967510e930e6bce343b82092ddfdb7ada62
Rodrigo Yuji Seto Soma <rodrigo.y.seto@gmail.com>
2026-01-08 19:59:34 +0900

    feat: implement quiz logic, question data and results screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,40 @@
 "use client";
 
 import { QuestionItem } from "@/components/QuestionItem";
+import { Results } from "@/components/Results";
 import { questions } from "@/data/questions";
 import { useState } from "react";
 
 const Page = () => {
+  const [answers, setAnswers] = useState<number[]>([]);
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showResult, setShowResult] = useState(false);
   const title = "Quiz";
 
-  const handleAnswered = (answer: number) => {};
+  const loadNextQuestion = () => {
+    if (questions[currentQuestion + 1]) {
+      setCurrentQuestion(currentQuestion + 1);
+    } else {
+      setShowResult(true);
+    }
+  };
+
+  const handleAnswered = (answer: number) => {
+    setAnswers([...answers, answer]);
+    loadNextQuestion();
+  };
 
   return (
     <div className="w-full h-screen flex justify-center items-center bg-gray-800">
       <div className="w-full max-w-xl rounded-md bg-gray-900 text-white shadow shadow-black">
         <div className="p-5 text-center font-bold text-3xl border-b border-gray-700">{title}</div>
         <div className="p-5">
-          <QuestionItem question={questions[currentQuestion]} count={currentQuestion + 1} onAnswer={handleAnswered} />
+          {!showResult && <QuestionItem question={questions[currentQuestion]} count={currentQuestion + 1} onAnswer={handleAnswered} />}
+          {showResult && <Results questions={questions} answers={answers} />}
         </div>
         <div className="p-5 text-center border-t border-gray-700">
-          {currentQuestion + 1} of {questions.length} question{questions.length === 1 ? "" : "s"}
+          {!showResult && `${currentQuestion + 1} of ${questions.length} question${questions.length === 1 ? "" : "s"}`}
+          {showResult && <button className="px-3 py-2 rounded-md bg-gray-700 text-white">Restart Quiz</button>}
         </div>
       </div>
     </div>
--- src/data/questions.ts
+++ src/data/questions.ts
@@ -6,4 +6,9 @@ export const questions: Question[] = [
     options: ["Flour", "Dye", "Water", "Sugar"],
     answer: 1,
   },
+  {
+    question: "What is the best cooking method?",
+    options: ["Microwave", "Stove", "Bonfire", "Flamethrower"],
+    answer: 3,
+  },
 ];
